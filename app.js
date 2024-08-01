c70a9b052c4a4891d912a9182ffdd2cbd425b6c6
Saul Gustavo <saulgustavo237@gmail.com>
2024-08-01 12:06:25 -0600

    Nuevo id

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 function validarEmail() {
-    let emailInput = document.getElementById('email-input');
-    var errorIcon = document.querySelector('.error-icono');
-    var errorMessage = document.querySelector('.error-msg');
+  let emailInput = document.getElementById('email');
+  var errorIcon = document.querySelector('.icon-error');
+  var errorMessage = document.querySelector('.error-text');
 
-    // Expresión regular para validar un formato de correo electrónico simple
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Expresión regular para validar un formato de correo electrónico simple
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (emailInput.value === '' || !emailRegex.test(emailInput.value)) {
-      // Muestra el icono de error y el mensaje de error
-      errorIcon.style.display = 'inline';
-      errorMessage.style.display = 'inline';
-      errorMessage.textContent = "Please provide a valid email";
-    } else {
-      // Oculta el icono de error y el mensaje de error si el correo es válido
-      errorIcon.style.display = 'none';
-      errorMessage.style.display = "inline";
-      // Aquí puedes enviar el formulario o realizar otras acciones
-      errorMessage.textContent = "Thanks for subscribe to our newsletter "
-    } 
+  if (emailInput.value === '' || !emailRegex.test(emailInput.value)) {
+    // Muestra el icono de error y el mensaje de error
+    errorIcon.style.display = 'inline';
+    errorMessage.style.display = 'inline';
+    errorMessage.textContent = "Please provide a valid email";
+  } else {
+    // Oculta el icono de error y el mensaje de error si el correo es válido
+    errorIcon.style.display = 'none';
+    errorMessage.style.display = "inline";
+    // Aquí puedes enviar el formulario o realizar otras acciones
+    errorMessage.textContent = "Thanks for subscribe to our newsletter "
   }
-  
+}
